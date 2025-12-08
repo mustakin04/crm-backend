c52978e47cc2fc0359bed740c43f6594f01a7148
mustakin04 <mustakinhasan37@gmail.com>
2025-12-08 12:09:56 +0600

    path update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://atlascrm.netlify.app",
+      "https://frabjous-ganache-551583.netlify.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
